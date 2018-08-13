3c7caf71e2767eecbd8686f98438568203b6a274
Bruno Paz <bruno.paz@jumia.com>
2018-08-13 12:16:46 +0100

    allow search on Markdown viewer

--- hawkeye/viewer/markdown.py
+++ hawkeye/viewer/markdown.py
@@ -12,7 +12,7 @@ gi.require_version('WebKit2', '4.0')
 from gi.repository import GObject, Gtk, Gdk, Gio, WebKit2 as WebKit
 from hawkeye import helpers
 
-class MarkdownViewer(WebKit.WebView):
+class MarkdownViewer(Gtk.VBox):
     """ Displays the file inside a webview """
 
     __gsignals__ = {
@@ -24,7 +24,10 @@ class MarkdownViewer(WebKit.WebView):
         """ constructor """
         super(MarkdownViewer, self).__init__()
 
-        settings = self.get_settings()
+        self.webview = WebKit.WebView()
+        self.build_search_bar()
+        
+        settings = self.webview.get_settings()
         settings.set_property('enable-developer-extras', True)
 
         self.load_assets()
@@ -39,10 +42,56 @@ class MarkdownViewer(WebKit.WebView):
 
         html = md.convert(md_string)
 
-        self.connect("decide-policy", self.on_decide_policy)
+        self.webview.connect("decide-policy", self.on_decide_policy)
 
-        self.load_html(html, '')
+        self.webview.load_html(html, '')
+        self.webview.show_all()
+        self.pack_start(self.search_bar, False, False, 0)
+        self.pack_start(self.webview, True, True, 0)
         self.show_all()
+
+        self.connect("key-press-event", self.on_key_pressed)
+
+    def on_key_pressed(self, widget, event):
+        """ Signal called on a key press inside of the main view """
+        
+        accel_mask = Gtk.accelerator_get_default_mod_mask()
+
+        # Show finder search bar on CTRL-F
+        # TODO this should be moved to Main window.
+        if event.state & accel_mask == Gdk.ModifierType.CONTROL_MASK and event.keyval == Gdk.KEY_f:
+          if self.search_bar.get_search_mode():
+            self.search_bar.set_search_mode(False)
+            self.search_entry.set_text("")
+          else:
+            self.search_bar.set_search_mode(True)
+
+
+    def build_search_bar(self):
+        """ Builds the search bar widget """
+        self.search_bar = Gtk.SearchBar()
+        self.search_entry = Gtk.SearchEntry(max_width_chars=45)
+        self.search_entry.connect("key-press-event", self.on_search)
+        self.search_bar.set_search_mode(False)
+        self.search_bar.connect_entry(self.search_entry)
+        self.search_bar.add(self.search_entry)
+        self.search_bar.set_show_close_button(True)
+
+    def on_search(self, widget, event):
+        """ Do a search on enter key """
+
+        if event.keyval != Gdk.KEY_Return:
+            return
+
+        script = """
+            var res = window.find("%s")
+            if (!res) {
+                alert("No results found!")
+            }
+        """
+
+        script = script % widget.get_text()
+        self.webview.run_javascript(script)
 
     def load_assets(self):
         """ Injects css and js files into the webview """
@@ -59,8 +108,8 @@ class MarkdownViewer(WebKit.WebView):
         script = WebKit.UserScript(
             hljs_script, WebKit.UserContentInjectedFrames.TOP_FRAME, WebKit.UserScriptInjectionTime.END, None, None)
 
-        self.get_user_content_manager().add_style_sheet(style)
-        self.get_user_content_manager().add_script(script)
+        self.webview.get_user_content_manager().add_style_sheet(style)
+        self.webview.get_user_content_manager().add_script(script)
 
     def on_decide_policy(self, webview, decision, decision_type):
         """ Intercepts requests """
